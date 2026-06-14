d26646c3b2419b6e47373aba60c1943af9f38202
kourain <maiquyen16503@gmail.com>
2026-06-14 14:20:48 +0700

    test performance

---
+++ tests/test_performance.rs
@@ -0,0 +1,30 @@
+mod controllers;
+use asp_dot_rust::{
+    ApplicationBuilder,
+    configuration::{CorsConfiguration, RateLimitConfiguration},
+    logging::LOGGER,
+};
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 20)]
+async fn test_application() {
+    LOGGER::set_enable(false);
+    // LOGGER::with_chrono_time_format("%Y-%m-%d %H:%M:%S%.9f");
+    // LOGGER::with_request_id(true);
+    let mut app_builder = ApplicationBuilder::new("TestApp");
+    app_builder.with_any_ip().with_http_port(8080);
+    app_builder
+        .add_custom_configuration(|config: &mut CorsConfiguration| {
+            config.allowed_origins = ["*"].into();
+            config.allowed_methods = [http::Method::GET, http::Method::POST].into();
+            config.allowed_headers = ["Content-Type"].into();
+        })
+        .add_custom_configuration(|cfg: &mut RateLimitConfiguration| {
+            cfg.max_requests = 5000000000;
+            cfg.limit_seconds = 1;
+            cfg.block_duration_seconds = 60;
+        });
+    app_builder.add_controllers();
+    let mut app = app_builder.build();
+    // app.use_cors().use_rate_limit();
+    let _ = app.run().await;
+}
