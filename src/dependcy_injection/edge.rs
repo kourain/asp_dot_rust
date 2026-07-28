763b1d7554c92b3af7ba2976db43fad13ac9ed62
kourain <maiquyen16503@gmail.com>
2026-07-29 01:12:14 +0700

    rm duplicate trait

--- src/dependcy_injection/edge.rs
+++ src/dependcy_injection/edge.rs
@@ -1,12 +1,6 @@
 pub use inventory;
 use std::any::TypeId;
 
-pub trait DependcyInjectableService: Send + Sync + 'static {
-    fn inject_service(service_scope: &crate::services::service_provider::service_provider_scope::ServiceProviderScope) -> Self
-    where
-        Self: Sized;
-}
-
 /// One cạnh trong đồ thị phụ thuộc: owner phụ thuộc vào danh sách dependencies
 pub struct DependencyEdge {
     pub owner: fn() -> TypeId,
