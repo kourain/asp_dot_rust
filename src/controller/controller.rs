7f1db4ac72328214f4371aac7c5186df8ecbadd6
kourain <maiquyen16503@gmail.com>
2026-07-29 22:10:39 +0700

    rm TODO

--- src/controller/controller.rs
+++ src/controller/controller.rs
@@ -1,7 +1,3 @@
 use crate::{http_context, utils::{ShareMutPtr, StructName}};
 pub type HttpContextRef = ShareMutPtr<http_context::HttpContext>;
 
-pub trait WithHttpContext: StructName {
-    fn new(http_context: HttpContextRef) -> Self;
-    // fn routing(&mut self);
-}
