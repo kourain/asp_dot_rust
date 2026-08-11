57bc1ee0d0f933a7dd7b45215dd837a260d2e92d
Quyen <85356599+kourain@users.noreply.github.com>
2026-08-11 06:55:29 +0000

    protector for share mut ptr

--- src/utils/share_mut_ptr.rs
+++ src/utils/share_mut_ptr.rs
@@ -1,19 +1,25 @@
-use std::ops::{Deref, DerefMut};
+use std::{
+    ops::{Deref, DerefMut},
+    sync::{Arc, atomic::AtomicBool},
+};
 
-pub struct ShareMutPtr<T>(*mut T);
+pub struct ShareMutPtr<T>(*mut T, Arc<AtomicBool>);
 
 unsafe impl<T> Send for ShareMutPtr<T> {}
 unsafe impl<T> Sync for ShareMutPtr<T> {}
 
 impl<T> ShareMutPtr<T> {
     pub fn new(value: &mut T) -> Self {
-        Self(value as *mut T)
+        Self(value as *mut T, Arc::new(AtomicBool::new(false)))
+    }
+    pub fn new_with_notify(value: &mut T, notify: Arc<AtomicBool>) -> Self {
+        Self(value as *mut T, notify)
     }
 }
 
 impl<T> Clone for ShareMutPtr<T> {
     fn clone(&self) -> Self {
-        panic!("ShareMutPtr cannot be cloned. It is a unique mutable pointer.");
+        Self(self.0, self.1.clone())
     }
 }
 
@@ -21,12 +27,20 @@ impl<T> Deref for ShareMutPtr<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
+        if self.1.load(std::sync::atomic::Ordering::Relaxed) {
+            let type_name = std::any::type_name::<T>();
+            panic!("{} of ShareMutPtr<T>: Target already drop", type_name);
+        }
         unsafe { &*self.0 }
     }
 }
 
 impl<T> DerefMut for ShareMutPtr<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
+        if self.1.load(std::sync::atomic::Ordering::Relaxed) {
+            let type_name = std::any::type_name::<T>();
+            panic!("{} of ShareMutPtr<T>: Target already drop", type_name);
+        }
         unsafe { &mut *self.0 }
     }
 }
